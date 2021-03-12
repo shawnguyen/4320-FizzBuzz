9c8a4f2b6995379611bbf636fcfc9946cd08e804
Shawn Nguyen <shawnnguyen@Shawns-MacBook-Pro.local>
2021-03-12 15:23:45 -0600

    Shortened code for numbers that are multiples of both 3 and 5 If Statement. Added comments to explain code.

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,13 @@
 for i in range(1,101):
-    if i % 3 == 0 and i % 5 == 0:
+    #If number is a multiple of 3 and 5
+    if i % 15 == 0:
         print ("FizzBuzz")
+    #If number is a multiple of 3
     elif i % 3 == 0:
         print ("Fizz")
+    #If number is a multiple of 5
     elif i % 5 == 0:
         print ("Buzz")
+    #Print number if number is not a multiple of 3, 5, or both.
     else:
         print (str(i))
